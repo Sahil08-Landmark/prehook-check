a551ce40ed046832636355a10f569daa9c694ff3
sahil0823 <sahil.sharma@amantyatech.com>
2023-07-31 07:11:32 +0000

    Updated file content via GitHub Actions

--- demo.cs
+++ demo.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Program{
     public static void Main(string []args){
-        var azure_key = "azure_key121";
+        var azure_key = "placeholder";
         var azure_key1 = "azure1234567";
         Console.WriteLine("Hello");
     }
