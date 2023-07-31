d1e7c7ebc91cc80b466b1a79e8425acb2f0e31bc
Sahil <sahil.sharma@amantyatech.com>
2023-07-31 10:17:46 +0530

    dwqd

--- demo.cs
+++ demo.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Program{
     public static void Main(string []args){
-        var obj = "azure_key";
+        var azure_key = "azure_key";
         Console.WriteLine("Hello");
     }
 }
