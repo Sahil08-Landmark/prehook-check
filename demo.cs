89319083fc1920395f30e9c955d876b2c1d3a87a
Sahil <sahil.sharma@amantyatech.com>
2023-09-27 12:53:15 +0530

    replace

--- demo.cs
+++ demo.cs
@@ -3,7 +3,8 @@ using System;
 public class Program{
     public static void Main(string []args){
         var azure_key = "pk_test_51HFzpqCvSyPvpfW3Fz1d6gP0eanK1Q0";
-        var azure_key1 = "sahil";
+        var azure_key1 = "fab13a0a-e027-4dcd-a2b7-568c195d86d6";
+        var azure_key2 = "NzBhMmY2NmUtZWE3Ny00ZTY0LThlMTEtNWNkZDdiNTcyMTg3";
         var ss = 1;
         Console.WriteLine("Hello");
     }
