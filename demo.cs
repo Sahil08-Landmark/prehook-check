c6da941725b794cbf89dfcbf2504d4c305808e4c
Sahil <sahil.sharma@amantyatech.com>
2023-09-27 12:36:42 +0530

    ...

--- demo.cs
+++ demo.cs
@@ -3,7 +3,7 @@ using System;
 public class Program{
     public static void Main(string []args){
         var azure_key = "AIDAQEAAAAAAAAAAAAAA";
-        var azure_key1 = "azure1234567";
+        var azure_key1 = "sahil";
         var ss = 1;
         Console.WriteLine("Hello");
     }
